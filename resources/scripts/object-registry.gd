b9b44d64da440a0b2301799e229223cfe20a7187
Darius Stonys <krotas@gmail.com>
2026-02-21 00:43:42 +0200

    Add milk+tv and calibrate object data
    Adjusted size, colliders, weight, score

--- resources/scripts/object-registry.gd
+++ resources/scripts/object-registry.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var scenes: Array[PackedScene] = [
 	preload("res://scenes/objects/kettle.tscn"),
+	preload("res://scenes/objects/milk.tscn"),
 	preload("res://scenes/objects/table.tscn"),
 	preload("res://scenes/objects/tomato.tscn"),
+	preload("res://scenes/objects/tv.tscn"),
 ]
