a84803a34871b70f848f5535074a1b8725316b32
Darius Stonys <krotas@gmail.com>
2026-02-22 01:21:53 +0200

    Add clock

--- resources/scripts/object-registry.gd
+++ resources/scripts/object-registry.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var scenes: Array[PackedScene] = [
+	preload("res://scenes/objects/clock.tscn"),
 	preload("res://scenes/objects/kettle.tscn"),
 	preload("res://scenes/objects/milk.tscn"),
 	preload("res://scenes/objects/table.tscn"),
